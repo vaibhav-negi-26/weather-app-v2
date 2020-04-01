5eb19f0253e528e648e0813af04f23e8535fb6ac
Vaibhav Negi <10vaibhavsinghnegi@gmail.com>
2020-04-02 01:10:36 +0530

    env setup and celsius fix

--- src/utility/forecast.js
+++ src/utility/forecast.js
@@ -1,18 +1,23 @@
 const request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const drakUrl = 'https://api.darksky.net/forecast/99840a089b5eac0c9a0a0049b2f18c42/' + lat + ',' + long
+    const drakUrl = 'https://api.darksky.net/forecast/' + process.env.DARK_SKY + '/' + lat + ',' + long
+    // console.log(drakUrl)
     request({
         url: drakUrl,
         json: true
-    }, (error, {body}) => {
+    }, (error, {
+        body
+    }) => {
         if (error) {
             callback("Unable to connect to weather service.", undefined)
         } else if (body.error) {
             callback("Unable to find location.", undefined)
         } else {
-            const result = `${body.daily.data[0].summary} Current temperature is ${body.currently.temperature} °F, humidity is ${body.daily.data[0].humidity} and ${body.currently.precipProbability}% chance of rain.`
-            callback(undefined,result)        
+            const temp = body.currently.temperature
+            const celcius = ((temp-32) * (5 / 9)).toFixed(2)
+            const result = `${body.daily.data[0].summary} Current temperature is ${celcius} °C, humidity is ${body.daily.data[0].humidity} and ${body.currently.precipProbability}% chance of rain.`
+            callback(undefined, result)
         }
     })
 }
--- src/utility/geocode.js
+++ src/utility/geocode.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZ2F1cmF2MjYyNiIsImEiOiJjazdhcHRmNnQxNmluM2RvZzNrdXR3dXBnIn0.TDuOGbocIKLoqPOkXrETuA&limit=1'
+    const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token='+process.env.MAP_BOX+'&limit=1'
+    // console.log(mapUrl)
     request({
         url: mapUrl,
         json: true
